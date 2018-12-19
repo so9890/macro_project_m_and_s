d7b36526316e4431fca8ce44c2fdde7a2dfa68a3
Dobkowitz <sdobkowitz@neori.uni-bonn.de>
2018-12-19 08:16:01 +0100

    First commit

---
+++ InfInequality/CEX_DATA_preparation.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 18 15:10:23 2018
+
+@author: sdobkowitz
+"""
+
+expen17_apa17_cex = pd.read_csv(r'C:\Users\sdobkowitz\Desktop\macro_topics\CEX_Data\expn17\apa17.csv', header = 0, sep=',' )

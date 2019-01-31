7730e8428821883abb54c00dd0acb08715084869
Dobkowitz <sdobkowitz@neori.uni-bonn.de>
2019-01-31 15:15:23 +0100

    Tests for cum_distribution function pass now.

--- InfInequality/test_CEX_DATA_preparation.py
+++ InfInequality/test_CEX_DATA_preparation.py
@@ -7,6 +7,7 @@ import pytest
 
 from functions import _cum_distribution
 from pandas.testing import assert_frame_equal, assert_series_equal
+from numpy.testing import assert_array_almost_equal  
 
 """ Test weighted_percentile.
 
@@ -33,7 +34,7 @@ n= setup['d']['FINLWT21'].sum()
 def expect_cum_distribution():
     """ This setup is to test if same values at the end are problematic. """
     out = {}
-    values = [5.6, 5.6, 6.6, 12, 18, 18, 18, 23, 26, 26, 29, 31, 34, 45, 45, 45]
+    values = [5.6, 5.6, 6.6, 12.0, 18.0, 18.0, 18.0, 23.0, 26.0, 26.0, 29.0, 31.0, 34.0, 45.0, 45.0, 45.0]
     out['d']  = pd.DataFrame(
         data=[values,
               [x / values[-1] for x in values]
@@ -46,11 +47,12 @@ def expect_cum_distribution():
 
 def test_cum_distribution_weights(setup_cum_distribution, expect_cum_distribution):
     calc_distribution = _cum_distribution(**setup_cum_distribution)
-    assert_frame_equal(calc_distribution['Cum_weights'].astype(float), expect_cum_distribution['d']['Cum_weights'])
+    assert_array_almost_equal(calc_distribution['Cum_weights'].values, expect_cum_distribution['d']['Cum_weights'].values)
 
 def test_cum_distribution_percentage(setup_cum_distribution, expect_cum_distribution):
     calc_distribution = _cum_distribution(**setup_cum_distribution)
-    assert_frame_equal(calc_distribution['Percentage_below_equal'].astype(float), expect_cum_distribution['d']['Percentage_below_equal'])
+    assert_array_almost_equal(calc_distribution['Percentage_below_equal'].values, expect_cum_distribution['d']['Percentage_below_equal'].values)
+
 
 ######################################################################
 """ The following is to debug the weighted_percentile function """
@@ -97,41 +99,3 @@ for i in range(0,len(d_sorted)):
             continue
 
        
-###################################################################################################    
-@pytest.fixture
-def expected_weighted_percentile():
-    
-    out = {}
-    out['d'] = pd.DataFrame(
-        data=[[-10, 0, 1, 2, 3, 4], [15, 15, 5, 30, 25, 10], [1, 2, ]]
-        columns={'VALUE','FINLWT21','Percentile'},
-    )
-    
-    return out
-
-
-
-""" Test function for several equal values in loop """
-
-@pytest.fixture
-def setup_same_values():
-
-    out = {}
-    out['d'] = pd.DataFrame(
-        data=[[2, 3, 2, 3, 3, -4], [3, 5, 1, 3, 2, 1]]).T
-    out['d'].columns= {'VALUE','FINLWT21'}
-
-    return out
-
-
-
-s = 0
-while d_sorted['VALUE'].iloc[i]==d_sorted['VALUE'].iloc[i+s]: 
-    
-    cum_weight += d_sorted['FINLWT21'].iloc[i+s]
-    s+=1 # s will thus have a value s.th. observation i+s is not yet included. 
-    
-d_sorted['Cum_weights'].iloc[i:i+s] = cum_weight # the end value is exlcuded! 
-d_sorted['Percentage_below'].iloc[i:i+s]= cum_weight/n
-
-

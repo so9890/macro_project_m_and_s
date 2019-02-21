e1b39fe325f7720d373fde6daf89616f225213fb
Mariam Petrosyan <44179698+mpetrosian@users.noreply.github.com>
2019-02-21 14:15:10 +0100

    stylize plot

--- analysis/7_LP_estimation.py
+++ analysis/7_LP_estimation.py
@@ -69,18 +69,21 @@ irf_st_dev = pd.DataFrame()
 
 irf_pval = pd.DataFrame()
 
-
+H=30
 for k in [stdev, gini_coeff, p90_p10, exp_10, exp_90]:
-    for h in range(21):
+    for h in range(H+1):
         y = k.shift(-h) - k.shift(-(h - 1))
         reg = sm.OLS(endog=y, exog=X[k.name], missing="drop").fit()
         irf_values.loc[h, k.name] = reg.params[1]
         irf_st_dev.loc[h, k.name] = reg.bse[1]
         irf_pval.loc[h, k.name] = reg.pvalues[1]
-    plt.plot(irf_values[k.name], label="irf")
-    plt.plot(1.65 * irf_st_dev[k.name], label="2_st_dev_up")
-    plt.plot(-1.65 * irf_st_dev[k.name], label="2_st_dev_down")
+    plt.plot(irf_values[k.name], label="irf",color='black',linewidth=2)
+    plt.plot(1.65 * irf_st_dev[k.name]+irf_values[k.name],'--', label="2_st_dev_up",color='grey')
+    plt.plot(-1.65 * irf_st_dev[k.name]+irf_values[k.name],'--', label="2_st_dev_down",color='grey')
+    x = np.arange(0,H+1)
+    plt.fill_between(x,1.65 * irf_st_dev[k.name]+irf_values[k.name],-1.65 * irf_st_dev[k.name]+irf_values[k.name],color='silver')
+    plt.plot([0]*H,color='red')
     plt.xlabel("time horizon")
     plt.ylabel(k.name)
-    plt.savefig("../out_figures/" + k.name)
+    plt.savefig("../out_figures/" + k.name,bbox_inches='tight')
     plt.clf()

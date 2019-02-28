37d047db3a7cd34abe01022040dd27b7c6597f51
mariam p <mpetrosyan@uni-bonn.de>
2019-02-28 18:17:12 +0100

    Seasonally adjusted

--- analysis/7_LP_estimation_agg_CPI.py
+++ analysis/7_LP_estimation_agg_CPI.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 import statsmodels.api as sm
 import matplotlib.pyplot as plt
+from statsmodels.tsa.seasonal import seasonal_decompose
 
 
 data = pd.read_pickle("../out_data_mngment/data_for_final_analysis_agg_cpi/data_inequality")
@@ -16,47 +17,44 @@ for c in data.columns[2:5]:
     plt.xticks(data["mm/yyyy"][::8], rotation=70)
     plt.savefig("../out_figures/CPIU_agg_app/time_series_of" + c + "_with_agg_CPI-U", bbox_inches="tight")
     plt.clf()
+#-------------------------
+# reset index to dat_time
+#------------------------
+data['Date'] = pd.to_datetime(data['mm/yyyy'])
+data = data.set_index('Date')
 
-stdev = data.loc[:157]["sd"]
+#---------------------------------------------------
+# Construct series for estimation. Adjust seasonally
+#---------------------------------------------------
+stdev = data.iloc[:158]["sd"]
 stdev.name = "stdev"
-plt.plot(data.loc[:157]["mm/yyyy"], stdev)
-plt.ylabel("st. dev")
-plt.xticks(data.loc[:157]["mm/yyyy"][::8], rotation=70)
-plt.savefig("../out_figures/CPIU_agg_app/real_ineq_stdev", bbox_inches="tight")
-plt.clf()
+stdev = stdev - seasonal_decompose(stdev, model='additive').seasonal
 
 
-gini_coeff = data.loc[:157]["Gini"]
+gini_coeff = data.iloc[:158]["Gini"]
 gini_coeff.name = "gini_coeff"
-plt.plot(data.loc[:157]["mm/yyyy"], gini_coeff)
-plt.ylabel("Gini")
-plt.xticks(data.loc[:157]["mm/yyyy"][::8], rotation=70)
-plt.savefig("../out_figures/CPIU_agg_app/real_ineq_Gini", bbox_inches="tight")
-plt.clf()
+gini_coeff = gini_coeff - seasonal_decompose(gini_coeff, model='additive').seasonal
 
 
-p90_p10 = data.loc[:157]["90-10"]
+p90_p10 = data.iloc[:158]["90-10"]
 p90_p10.name = "p90_p10"
-plt.plot(data.loc[:157]["mm/yyyy"], p90_p10)
-plt.ylabel("p90-p10")
-plt.xticks(data.loc[:157]["mm/yyyy"][::8], rotation=70)
-plt.savefig("../out_figures/CPIU_agg_app/real_ineq_p90-p10", bbox_inches="tight")
-plt.clf()
+p90_p10 = p90_p10 - seasonal_decompose(p90_p10, model='additive').seasonal
 
 
-exp_10 = pd.read_pickle("../out_data_mngment/data_for_final_analysis_agg_cpi/log_exp_series_p10")
+
+exp_10 = pd.read_pickle("../out_data_mngment/data_for_final_analysis/log_exp_series_p10")
 exp_10 = exp_10.sort_values(by=["year", "month"])
-exp_10 = exp_10["exp_p1-p10_het"]
-exp_10.index = range(len(exp_10))
-exp_10 = exp_10[:158]
+exp_10 = exp_10.iloc[:158]["exp_p1-p10_het"]
+exp_10.index = data.index[:158]
+exp_10 = exp_10 - seasonal_decompose(exp_10, model='additive').seasonal
 
 
-exp_90 = pd.read_pickle("../out_data_mngment/data_for_final_analysis_agg_cpi/log_exp_series_p90")
+
+exp_90 = pd.read_pickle("../out_data_mngment/data_for_final_analysis/log_exp_series_p90")
 exp_90 = exp_90.sort_values(by=["year", "month"])
-exp_90 = exp_90["exp_p90-p100_het"]
-exp_90.index = range(len(exp_90))
-exp_90 = exp_90[:158]
-
+exp_90 = exp_90.iloc[:158]["exp_p90-p100_het"]
+exp_90.index = data.index[:158]
+exp_90 = exp_90 - seasonal_decompose(exp_90, model='additive').seasonal
 
 shock_series = (
     pd.read_excel(
@@ -64,11 +62,13 @@ shock_series = (
     )
     * 100
 )
+    
+shock_series.index=data.index[:158]
+
+shock_series = shock_series - seasonal_decompose(shock_series, model='additive').seasonal
 
 
-# shock_series = pd.concat(
-#    [data.loc[0:157]["year"], data.loc[0:157]["month"], shock_series], axis=1
-# )
+
 
 # create data frame of regressors (don't changes with horizon h) for each ineq. measure
 
@@ -76,6 +76,7 @@ X = {}
 for k in [stdev, gini_coeff, p90_p10, exp_10, exp_90]:
     X[k.name] = pd.DataFrame()
     X[k.name]["const"] = np.ones(len(shock_series))
+    X[k.name].index = k.index
     for i in range(20):
         X[k.name]["rr_L" + str(i)] = shock_series["Shock_values"].shift(i)
     for j in range(1, 7):
